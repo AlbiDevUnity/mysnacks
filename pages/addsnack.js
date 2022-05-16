4b7fab5a4aec932994491dda523b36ff22ac2375
5AI.RAVASIOAL.03 <5AI.RAVASIOAL.03@LAB_2L5_PC11>
2022-05-16 13:04:30 +0200

    add non va

--- pages/addsnack.js
+++ pages/addsnack.js
@@ -1,11 +1,53 @@
 import Navbar from "../components/NavBar";
 
-export default function Page(){
+export default function Page() {
 
-    return(
-        <>
-        <Navbar></Navbar>
-        
-        </>
-    )
+    async function onSubmitForm(event){
+
+        event.preventDefault()
+        const data = {
+            nome: event.target.nome.value,
+            tipologia: event.target.tipologia.value,
+            quantita: event.target.quantita.value,
+            prezzo: event.target.prezzo.value,           
+          }
+
+        const response = await fetch("/api/snack", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          })
+
+          const snack = await response.json()
+    }
+  return (
+    <>
+      <Navbar></Navbar>
+
+      <form method="POST" onSubmit={onSubmitForm    }>
+        <div className="form-group">
+          <label htmlFor="nomeInput">Nome</label>
+          <input type="text" className="form-control" id="nomeInput" name="nome"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="TipologiaInput">Tipologia</label>
+          <input type="text" className="form-control" id="TipologiaInput" name="tipologia"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="QuantitaInput">Quantità</label>
+          <input type="numeric" className="form-control" id="QuantitaInput" name="quantita"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="prezzoInput">Prezzo</label>
+          <input type="numeric" className="form-control" id="prezzoInput" name="prezzo"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
